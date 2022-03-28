fa5af6aa5c963e6592a951b9a70487c8004e22ca
Fernando Fernández <heisenferg@gmail.com>
2022-03-28 20:13:03 +0200

    Se ve y carga

--- app/src/main/java/com/example/stalingrad/MainActivity.java
+++ app/src/main/java/com/example/stalingrad/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.stalingrad;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,19 +13,47 @@ public class MainActivity extends AppCompatActivity {
 
     Button start;
     CargaJuego carga;
+    private Juego j;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        j = new Juego(this);
         setContentView(R.layout.activity_main);
-        carga = new CargaJuego();
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        hideSystemUI();
         start = findViewById(R.id.buttonStart);
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(), CargaJuego.class);
-                startActivity(intent);
+                setContentView(j);
             }
         });
     }
+
+
+    private void hideSystemUI() {
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.HONEYCOMB) {
+            //A partir de kitkat
+            j.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+            //cuando se presiona volumen, por ej, se cambia la visibilidad, hay que volver
+            //a ocultar
+            j.setOnSystemUiVisibilityChangeListener(new
+                                                            View.OnSystemUiVisibilityChangeListener() {
+                                                                @Override
+                                                                public void onSystemUiVisibilityChange(int visibility) {
+                                                                    hideSystemUI();
+                                                                }
+                                                            });
+        }
+    }
+
+
 }
