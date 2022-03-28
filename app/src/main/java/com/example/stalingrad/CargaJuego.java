df91275d7971df419a8312e1d7fc22fb15348fcb
Fernando Fernández <heisenferg@gmail.com>
2022-03-28 20:20:51 +0200

    01 - Se ve y carga

--- app/src/main/java/com/example/stalingrad/CargaJuego.java
+++ app/src/main/java/com/example/stalingrad/CargaJuego.java
@@ -18,29 +18,6 @@ public class CargaJuego extends AppCompatActivity {
         j = new Juego(this);
         setContentView(j);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-        hideSystemUI();
-    }
-
-    private void hideSystemUI() {
-        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.HONEYCOMB) {
-            //A partir de kitkat
-            j.setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
-                            | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
-                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-            //cuando se presiona volumen, por ej, se cambia la visibilidad, hay que volver
-            //a ocultar
-            j.setOnSystemUiVisibilityChangeListener(new
-                                                            View.OnSystemUiVisibilityChangeListener() {
-                                                                @Override
-                                                                public void onSystemUiVisibilityChange(int visibility) {
-                                                                    hideSystemUI();
-                                                                }
-                                                            });
-        }
     }
 
 
